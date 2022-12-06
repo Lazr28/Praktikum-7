8e71e16838e3cb50ac5c5002e909536246474fff
Lazr28 <dev.rizalm@gmail.com>
2022-12-06 06:23:19 -0800

    Update

--- def.py
+++ def.py
@@ -31,22 +31,30 @@ def Lihat ():
             print('| {0:^3}| {1:11} | {2:<17} | {3:7} | {4:4} | {5:3} | {6:7.2f} |'.format(x, i, dataMhs[i]["nama"], dataMhs[i]["tugas"], dataMhs[i]["uts"], dataMhs[i]["uas"], dataMhs[i]["akhir"]))
             x += 1
     print("=========================================================================")
-def cariNIM():
-    print("Cari")    
-
-print("                         Program Input Nilai")
-print("=========================================================================")
-while True:
-    menu = input("(L)ihat, (T)ambah, (U)bah, (H)apus, (C)ari, (K)eluar : ")
-    if menu.capitalize() == "T":
-        Tambah()
-        
-    if menu.capitalize() == "L":
-        Lihat()
-    if menu.capitalize() == "U":
-        print("Mengubah data Mahasiswa")
-        nim = input("Masukan NIM          : ")
+def cariNIM(nim):
+        Jdl_Tbl()
         if (dataMhs.get(nim, "kosong") != "kosong"):
+            print('| {0:^3}| {1:11} | {2:<17} | {3:7} | {4:4} | {5:3} | {6:6.2f} |'.format(
+                1, nim, dataMhs[nim]["nama"], dataMhs[nim]["tugas"], dataMhs[nim]["uts"], dataMhs[nim]["uas"], dataMhs[nim]["akhir"]))
+        else:
+            print("|                 DATA MAHASISWA DENGAN NO NIM ",
+                  nim, "KOSONG / TIDAK ADA     |")
+        print("=========================================================================")
+def cariNama(nama):
+    x,found = 0,0
+    Jdl_Tbl()
+    for i, j in dataMhs.items():
+        x+=1
+        
+        if ((dataMhs.get(i)).get('nama').startswith(nama)):
+            found += 1
+            print('| {0:^3}| {1:11} | {2:<17} | {3:7} | {4:4} | {5:3} | {6:6.2f} |'.format(
+                1, i, dataMhs[i]["nama"], dataMhs[i]["tugas"], dataMhs[i]["uts"], dataMhs[i]["uas"], dataMhs[i]["akhir"]))
+    if (found ==0):
+        print("Data tidak ada")
+    print("=========================================================================")
+def Ubah(nim):
+    if (dataMhs.get(nim, "kosong") != "kosong"):
             nama = input("Masukan Nama         : ")
             if nama == "":
                 nama = dataMhs[nim]['nama']
@@ -64,8 +72,21 @@ while True:
             dataMhs[nim] = {"nama": nama, "tugas": tugas,
                             "uts": uts, "uas": uas, "akhir": akhir}
             print("Berhasil Mengubah Data")
-        else:
+    else:
             print("Data tidak ditemukan.\nPastikan anda memasukan NIM yang benar ")
+            
+print("                         Program Input Nilai")
+print("=========================================================================")
+while True:
+    menu = input("(L)ihat, (T)ambah, (U)bah, (H)apus, (C)ari, (K)eluar : ")
+    if menu.capitalize() == "T":
+        Tambah()
+    if menu.capitalize() == "L":
+        Lihat()
+    if menu.capitalize() == "U":
+        print("Mengubah data Mahasiswa")
+        nim = input("Masukan NIM          : ")
+        Ubah(nim)
 
     if menu.capitalize() == "H":
         print("Menghapus data Mahasiswa")
@@ -78,17 +99,17 @@ while True:
 
     if menu.capitalize() == "C":
         print("Mencari Nilai Mahasiswa")
-        nim = input("Masukan Nim : ")
-        Jdl_Tbl()
-        if (dataMhs.get(nim, "kosong") != "kosong"):
-            print('| {0:^3}| {1:11} | {2:<17} | {3:7} | {4:4} | {5:3} | {6:6.2f} |'.format(
-                1, nim, dataMhs[nim]["nama"], dataMhs[nim]["tugas"], dataMhs[nim]["uts"], dataMhs[nim]["uas"], dataMhs[nim]["akhir"]))
-        else:
-            print("|                 DATA MAHASISWA DENGAN NO NIM ",
-                  nim, "KOSONG / TIDAK ADA     |")
-        print("=========================================================================")
+        SearchBy = input("Cari berdasarkan 1. NIM atau 2. Nama :")
+        if SearchBy == "1" :
+            nim = input("Masukan NIM :")
+            cariNIM(nim)
+        elif SearchBy == "2" :
+            nama = input("Masukan Nama : ")
+            cariNama(nama)
+            
     if menu.capitalize() == "K":
         print("Keluar dari program..")
         break
-    elif menu.capitalize() not in ('L', 'T', 'U', 'H', 'C', 'K'):
+    
+    elif menu.capitalize() not in ('L', 'T', 'U', 'H', 'C','K'):
         print("Mohon Pilih menu yang benar!")
